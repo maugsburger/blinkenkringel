2a859f59d23fbe855d641849fc8101975c83f5f9
Moritz Augsburger <moritz@augsburger.name>
2014-01-15 13:29:22 +0000

    simple idle sleep mode

--- sleep.c
+++ sleep.c
@@ -1,3 +1,12 @@
-#include <avr/io.h>
-#include <avr/interrupt.h>
-#include <sleep.h>
+#include <avr/sleep.h>
+#include "sleep.h"
+
+void sleep_idle( void ){
+    set_sleep_mode(SLEEP_MODE_IDLE);
+    sleep_mode();   
+}
+
+void sleep_powerdown( void ){
+    set_sleep_mode(SLEEP_MODE_PWR_DOWN);
+    sleep_mode();
+}
--- sleep.h
+++ sleep.h
@@ -1,5 +1,8 @@
 #ifndef _SLEEP_H
 #define _SLEEP_H
 
+void sleep_idle( void );
+
+void sleep_powerdown( void );
 
 #endif // _SLEEP_H
