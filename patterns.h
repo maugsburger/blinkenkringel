86bfcd9a3d25623ca63c55383637f3c753ca336b
Moritz Augsburger <moritz@augsburger.name>
2014-01-15 12:29:52 +0000

    pwm test pattern added

--- patterns.h
+++ patterns.h
@@ -27,6 +27,10 @@ const __flash pattern_element lpe_1300_400 = { 0x13, 0x00, 70/5 };
 
 const __flash pattern_element lpe_1000_400 = { 0x10, 0x00, 200/5 };
 
+const __flash pattern_element lpe_1000_5 = { 0x10, 0x00, 1 };
+const __flash pattern_element lpe_2000_5 = { 0x20, 0x00, 1 };
+const __flash pattern_element lpe_3000_5 = { 0x30, 0x00, 1 };
+const __flash pattern_element lpe_4000_5 = { 0x40, 0x00, 1 };
 
 
 const __flash pattern_element * const __flash lp_elements_dualflash[] = { 
@@ -45,13 +49,22 @@ const __flash pattern_element * const __flash lp_elements_single_dim[] = {
     &lpe_1000_400
 };
 
+const __flash pattern_element * const __flash lp_elements_single_upramp[] = { 
+    &lpe_1000_5, &lpe_2000_5, &lpe_3000_5, &lpe_4000_5, &lpe_0000_400
+};
+
+
+
+
 const __flash light_pattern lp_dualflash = { 4, 1, &lp_elements_dualflash };
 const __flash light_pattern lp_dualflash_s = { 4, 2, &lp_elements_dualflash_split };
 const __flash light_pattern lp_rotate = { 4, 4, &lp_elements_rotate };
 const __flash light_pattern lp_rotate_dim = { 1, 4, &lp_elements_single_dim };
 const __flash light_pattern lp_stand_dim = { 1, 1, &lp_elements_single_dim };
+const __flash light_pattern lp_stand_upramp = { 5, 1, &lp_elements_single_upramp };
 
 const __flash light_pattern *light_patterns[] = {
+    &lp_stand_upramp,
     &lp_stand_dim,
     &lp_rotate_dim,
     &lp_dualflash,
